014be415e9e07e998aff4c7d5316898e6ca2f401
lia978 <lia978@gmail.com>
2018-05-10 10:13:18 -0400

    modified readme

--- inst/package_make/build.R
+++ inst/package_make/build.R
@@ -5,11 +5,12 @@
 #install_github('hadley/staticdocs')
 
 require(devtools)
-require(staticdocs)
+#require(staticdocs)
+require(pkgdown)
 
 #package.dir <- normalizePath("../../../Gistic2GE")
-package.dir <-"/Users/amyli/Desktop/git_projects/iEDGE"
-
+#package.dir <-"/Users/amyli/Desktop/git_projects/iEDGE"
+package.dir<-"../../../iEDGE"
 
 
 
@@ -22,8 +23,10 @@ build(package.dir)
 cat("checking package...\n")
 check(package.dir) #package checking
 #check(package.dir, args = "--no-examples")
+
 cat("loading package...\n")
 load_all(package.dir) #building package
+
 cat("library package...\n")
 library(iEDGE)
 
@@ -39,6 +42,6 @@ library(iEDGE)
 #generate html pages
 setwd(package.dir)
 cat("creating html help pages for package...\n")
-build_site(pkg = package.dir, examples = TRUE, launch = TRUE)
+build_site(pkg = "./", examples = TRUE)
 
 
